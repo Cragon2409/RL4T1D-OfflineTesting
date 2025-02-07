9d9eae35ee47e60d8e9616308ca82bc89ce06328
Samuel Price <u7482502@anu.edu.au>
2025-02-08 10:16:03 +1100

    added import for folder type data

--- data/import_data.py
+++ data/import_data.py
@@ -1,11 +1,12 @@
 import os
 import torch
 import numpy as np
-import csv
 import pandas as pd
 from datetime import datetime
 import sys
 import pickle
+import json
+from omegaconf import DictConfig, OmegaConf
 
 AGE_VALUES = ["adolescent", "adult"]
 MODEL_TYPES = ["A2C", "AUXML", "BBHE", "BBI", "G2P2C", "PPO", "SAC"]
@@ -16,7 +17,15 @@ CSV_TYPE_MODELS = ["BBHE", "BBI"]
 OBJECT_SAVE_FILE = "../data" + "/object_save/data_dictionary.pkl"
 
 
+"""
+Ideas:
+- Might want to add a column for patient id
+
+"""
 EXCLUDE_FILES = ["quadratic.csv", "real.csv"]
+
+EXCLUDE_IN_FILES = "summary"
+
 def import_all_data(
         dest="../data", 
         age_range = AGE_VALUES,
@@ -56,15 +65,37 @@ def import_all_data(
                 for folder in available_folders:
                     folder_dest = model_folder + folder
                     print('\t' + folder_dest)
+                    args = open_arg_file(folder_dest + '/args.json')
+                    #taking validation and testing together in one list
+                    for trial_folder in ['/testing/data/', '/training/data/']:
+                        trial_folder_dest = folder_dest + trial_folder
+                        available_files = os.listdir(trial_folder_dest)
+                        for file in available_files:
+                            if not EXCLUDE_IN_FILES in file:
+                                file_dest = trial_folder_dest + file
+                                data_dict[age][model].append(import_from_big_csv(file_dest))
+
+
                 # data_dict[age][model] = np.array(data_dict)
 
     return data_dict
 
-CSV_HEADERS = ["CGM", "meal", "ins", "t"]
+CSV_HEADERS = ["cgm", "meal", "ins", "t"]
 def import_from_csv(file_dest, headers=CSV_HEADERS): #imports data from a csv file
     df = pd.read_csv(file_dest, header=None, names=headers)
     data_dict = {col: df[col].to_numpy(dtype=float) for col in headers}
     return data_dict
+
+def import_from_big_csv(file_dest, columns=["cgm","meal","rl_ins","t"]):
+    df = pd.read_csv(file_dest, usecols=columns)
+    data_dict = {col: df[col].to_numpy(dtype=float) for col in columns}
+
+    data_dict["ins"] = data_dict["rl_ins"]
+    del data_dict["rl_ins"]
+
+    return data_dict
+
+
 
 def import_from_obj(file_dest=OBJECT_SAVE_FILE):
     with open(file_dest, 'rb') as f:
@@ -76,14 +107,22 @@ def save_to_obj_file(data_dict, file_dest=OBJECT_SAVE_FILE):
         data_dict = pickle.dump(data_dict, f)
     return data_dict
 
+def open_arg_file(file_dest):
+    with open(file_dest,'r') as fp:
+        args_dict = json.load(fp)
+        fp.close()
+    return OmegaConf.create(args_dict)
     
-SAVE_TO_PICKLE = True
-READ_FROM_PICKLE = False
 if __name__ == "__main__":
+    SAVE_TO_PICKLE = False
+    READ_FROM_PICKLE = False
+
     if READ_FROM_PICKLE:
         start_time = datetime.now()
 
+
         file_dest="data/object_save/data_dictionary.pkl"
+        print("Starting read from file",file_dest)
         data = import_from_obj(file_dest)
 
         end_time = datetime.now()
@@ -96,12 +135,11 @@ if __name__ == "__main__":
         file_dest="data/object_save/data_dictionary.pkl"
         
         data = import_all_data("data")
-        # print(import_from_csv("data/adult/BBI/logs_worker_20_0.csv"))
         print("\nSuccesfully imported.")
 
         end_time = datetime.now()
         duration = end_time - start_time
-        print("Executed in",duration.total_seconds(), "seconds")
+        print(f"Executed in {int(duration.total_seconds() // 60)}m {duration.total_seconds() % 60 :.1f}s")
 
         obj_size = sys.getsizeof(data)
         obj_size_mb = obj_size / (1024 ** 2)
@@ -111,8 +149,9 @@ if __name__ == "__main__":
             for model_k in data[age_k]:
                 print("\tModel:", model_k, "with length", len(data[age_k][model_k]))
         
-        save_to_obj_file(data, file_dest)
-        file_size = os.path.getsize(file_dest)
-        print(f"Object saved to {file_dest} with size {file_size / (1024 * 1024):.2f}MB")
+        if SAVE_TO_PICKLE:
+            save_to_obj_file(data, file_dest)
+            file_size = os.path.getsize(file_dest)
+            print(f"Object saved to {file_dest} with size {file_size / (1024 * 1024):.2f}MB")
         
 
